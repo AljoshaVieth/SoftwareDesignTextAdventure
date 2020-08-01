61c3d7c66b4fd1a393c85b7b2012fc156556381d
Aljosha <aljoshajustin@gmail.com>
2020-08-01 23:57:29 +0200

    updated main

--- game/scripts/game.js
+++ game/scripts/game.js
@@ -1,4 +1,19 @@
 "use strict";
-let consoleInput = document.getElementById("consoleInput");
-consoleInput.addEventListener('blur', () => consoleInput.focus());
+var game;
+(function (game) {
+    let consoleInput = document.getElementById("consoleInput");
+    consoleInput.addEventListener('blur', () => consoleInput.focus());
+    consoleInput.addEventListener('keydown', function (event) {
+        if (event.which == 13 || event.keyCode == 13) {
+            game.GameManager.readInput();
+        }
+    });
+    console.log("Creating default gameState");
+    let state = game.GameManager.createDefaultGame("test");
+    console.log("finished");
+    console.log("printing gamestate...");
+    game.GameManager.saveGame();
+    console.log("Starting game...");
+    game.GameManager.startGame(state);
+})(game || (game = {}));
 //# sourceMappingURL=game.js.map
--- game/scripts/game.ts
+++ game/scripts/game.ts
@@ -1,2 +1,22 @@
-let consoleInput: HTMLInputElement = <HTMLInputElement> document.getElementById("consoleInput");
-consoleInput.addEventListener('blur', () => consoleInput.focus());
+
+namespace game {
+    let consoleInput: HTMLInputElement = <HTMLInputElement> document.getElementById("consoleInput");
+    consoleInput.addEventListener('blur', () => consoleInput.focus());
+    consoleInput.addEventListener('keydown', function (event) {
+        if (event.which == 13 || event.keyCode == 13) {
+            GameManager.readInput();
+        }
+    });
+    console.log("Creating default gameState")
+    let state: GameState = GameManager.createDefaultGame("test");
+    console.log("finished");
+    console.log("printing gamestate...");
+
+    GameManager.saveGame();
+
+    console.log("Starting game...")
+    GameManager.startGame(state);
+}
+
+
+
