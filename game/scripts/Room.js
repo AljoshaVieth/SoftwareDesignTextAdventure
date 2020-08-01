7b0a4180549f0b27e1efab05637f93ea2af7f9d2
Aljosha <aljoshajustin@gmail.com>
2020-08-01 23:57:59 +0200

    altered the way the room description is generated

--- game/scripts/Room.js
+++ game/scripts/Room.js
@@ -5,14 +5,34 @@ var game;
         constructor(_name, _description, _inventory, _people, _adjacentRooms) {
             super();
             this.name = _name;
-            this.description = _description;
             this.inventory = _inventory;
             this.people = _people;
             this.adjacentRooms = _adjacentRooms;
+            this.description = _description;
+            this.updateLookDescription();
+            console.log("Creating room with following adjacentRooms: " + _adjacentRooms.size);
+            let output = "Rooms: ";
+            _adjacentRooms.forEach(function (room, direction) {
+                output = output + direction + room.name;
+            });
+            console.log(output);
         }
         getRandomAdjacentRoom() {
             let rooms = Array.from(this.adjacentRooms.values());
             return rooms[Math.floor(Math.random() * rooms.length)];
+        }
+        updateLookDescription() {
+            let allItems = "Items[ ";
+            this.inventory.forEach(function (item) {
+                allItems = allItems + item.name + " | ";
+            });
+            allItems = allItems + " ]";
+            let allHumans = "Humans[ ";
+            this.people.forEach(function (human) {
+                allHumans = allHumans + human.name;
+            });
+            allHumans = allHumans + " ]";
+            this.lookDescription = this.description + " :  " + allItems + " _ " + allHumans;
         }
     }
     game.Room = Room;
--- game/scripts/Room.ts
+++ game/scripts/Room.ts
@@ -1,22 +1,50 @@
 namespace game {
-    export class Room extends Describable{
+    export class Room extends Describable {
         public inventory: Item[];
         public people: Human[];
         public adjacentRooms: Map<Direction, Room>;
+        public lookDescription: string;
 
 
         constructor(_name: string, _description: string, _inventory: Item[], _people: Human[], _adjacentRooms: Map<Direction, Room>) {
             super();
             this.name = _name;
-            this.description = _description;
             this.inventory = _inventory;
             this.people = _people;
             this.adjacentRooms = _adjacentRooms;
+            this.description = _description;
+            this.updateLookDescription();
+
+            console.log("Creating room with following adjacentRooms: " + _adjacentRooms.size);
+            let output: string = "Rooms: ";
+            _adjacentRooms.forEach(function (room, direction) {
+                output = output + direction + room.name;
+
+            })
+            console.log(output);
         }
 
         getRandomAdjacentRoom(): Room {
             let rooms = Array.from(this.adjacentRooms.values());
             return rooms[Math.floor(Math.random() * rooms.length)];
         }
+
+        updateLookDescription(): void {
+            let allItems: string = "Items[ ";
+            this.inventory.forEach(function (item) {
+                allItems = allItems + item.name + " | ";
+
+            })
+            allItems = allItems + " ]";
+
+            let allHumans: string = "Humans[ ";
+            this.people.forEach(function (human) {
+                allHumans = allHumans + human.name;
+
+            })
+            allHumans = allHumans + " ]";
+            this.lookDescription = this.description + " :  " +  allItems  + " _ " + allHumans;
+        }
+
     }
 }
